c61cb3b0124d4cd386917b4721b7a81e360875f2
AnasShar <Anas10895@gmail.com>
2019-09-05 14:53:47 +0300

    adding game logic with &&

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,21 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+const cardInPlay = [];
+
+var cardOne = cards[3];
+cardInPlay.push(cardOne)
+console.log("User flipped " + cardOne );
+
+
+var cardTow = cards[1];
+cardInPlay.push(cardTow)
+console.log("User flipped " + cardTow );
+
+if (cardInPlay.length === 2 && cardInPlay[0] === cardInPlay[1]){
+	alert("you found a match !! ");
+
+}
+ else {
+ 	alert("Sorry, try again ")
+ }
+
