cdf43ff1103ca3ef1cd06a767911cd5fa80b179f
AnasShar <Anas10895@gmail.com>
2019-09-11 18:52:07 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,54 @@
 
-var cards = ["queen", "queen", "king", "king"];
+
 var cardInPlay = [];
+var cards =[
+	{
+		rank:'queen',
+		suit:'hearts',
+		cardImage:"images/queen-of-hearts.png"
+	},
+	{
+		rank:'queen',
+		suit:'diamonds',
+		cardImage:"images/queen-of-diamonds.png"
+
+	},
+	{
+		rank:'king',
+		suit:'hearts',
+		cardImage:"images/king-of-hearts.png"
+	},
+	{
+		rank:'king',
+		suit:'diamonds',
+		cardImage:"images/king-of-diamonds.png"
+	}
+
+];
+
 
 function checkForMatch(){
 	if (cardInPlay[0] === cardInPlay[1]){
 		console.log("you found a match !! ")
+
 	}
 
 	 else {
 	 	console.log("Sorry, try again ");
 	}
-}
+};
 
 function flipCard(cardId){
-console.log("User flipped " + cards[cardId]);
-	cardInPlay.push(cards[cardId]);
+	console.log(cards[cardId].suit )
+	console.log(cards[cardId].cardImage )
+console.log("User flipped " + cards[cardId].rank);
+	cardInPlay.push(cards[cardId].rank);
 if (cardInPlay.length === 2){
 checkForMatch();
 }
 
-}
+};
 
 
-flipCard(0);
-flipCard(1);
+flipCard(3);
+flipCard(2);
